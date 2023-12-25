ed28c717928e0c4727c2c3f5270e03150b990a43
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-12-25 18:48:09 +0200

    Implement collision detection

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 
-#include<windows.h>
-#include<stdio.h>
-#include<iostream>
-#include<GL/glut.h>
-#include<string>
-#include<sstream>
-
-
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <GL/glut.h>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
+bool checkCollision(float aX, float aY, float aW, float aH, float bX, float bY, float bW, float bH);
 
-int main(int argc, char** argv){
-
-    
-
+int main(int argc, char **argv)
+{
 
     return 0;
+}
+
+
+bool checkCollision(float aX, float aY, float aW, float aH, float bX, float bY, float bW, float bH)
+{
+    if (aY + aH < bY)
+        return false;
+    else if (aY > bY + bH)
+        return false;
+    else if (aX + aW < bX)
+        return false;
+    else if (aX > bX + bW)
+        return false;
+    else
+        return true;
 }
