316240cbee8e406de5468c85af8bec061dc49c7f
Abdo Tolba <DevAbdoTolba@gmail.com>
2023-12-25 18:43:13 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include<windows.h>
+#include<stdio.h>
+#include<iostream>
+#include<GL/glut.h>
+#include<string>
+#include<sstream>
+
+
+
+using namespace std;
+
+
+int main(int argc, char** argv){
+
+    
+
+
+    return 0;
+}
