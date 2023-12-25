ae72171e52b7ed1907a0f2f32fcdd56835aa8a34
amr-elbana <elbana544@gmail.com>
2023-12-25 19:10:25 +0200

    Implement Game display

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ using namespace std;
   
   
 void myInit (void);
-
+void myDisplay(void);
   
   // ================================================================================================== //
  // ==================================== Global Variables ============================================ //
@@ -48,4 +48,41 @@ void myInit (void){
     glLoadIdentity();
     glOrtho(0.0, 760.0, 480.0, 0.0, -1.0, 1.0);
 
+}
+
+void myDisplay(void){
+    glClear (GL_COLOR_BUFFER_BIT);
+    glColor3f (0.0, 0.0, 0.0);
+    //Bar
+    glBegin(GL_QUADS);
+    glColor3ub(255, 204, 102);
+    glVertex2f(barX, barY);
+    glVertex2f(barX+barWidth, barY);
+    glVertex2f(barX+barWidth, barY+barheight);
+    glVertex2f(barX, barY+barheight);
+    glEnd();
+    //Ball
+    glBegin(GL_QUADS);
+    glColor3ub(255, 0, 0);
+    glVertex2f(ballX, ballY);
+    glVertex2f(ballX+ballWH, ballY);
+    glVertex2f(ballX+ballWH, ballY+ballWH);
+    glVertex2f(ballX, ballY+ballWH);
+    glEnd();
+
+    //sidebar
+    glBegin(GL_QUADS);
+    glColor3ub(0, 0, 0);
+    glVertex2f(480, 0);
+    glVertex2f(480, 480);
+    glVertex2f(485, 480);
+    glVertex2f(485, 0);
+    glEnd();
+
+    print(score);
+    createBricks();
+    message(flag);
+    completeMessage(flag2);
+
+    glutSwapBuffers();
 }
